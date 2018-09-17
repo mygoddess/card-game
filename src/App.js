1bd7fe8bf318a56139729a8016497720a8c79879
Wansit Siriluck <mygoddess@gmail.com>
2018-09-17 10:54:50 +0700

    Initial card game

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+import WordCard from './WordCard';
+
+const word = "Hello";
+
+class App extends Component {
+  
+  render() {
+    return (
+      <div>
+        {
+          <WordCard value="hello"/>
+        }
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/CharacterCard.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react';
+export default class Charactercard extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+            active: false,
+        }
+    }
+    activate = () => {
+        if(!this.state.active){
+            this.setState({active: true})
+        }
+    }
+    render(){
+        let className = `card ${this.state.active ? 'activeCard' : ''}`
+        return (
+            <div className={className} onClick={this.activate}>
+                {this.props.value}
+            </div>
+
+        )
+    }
+}
